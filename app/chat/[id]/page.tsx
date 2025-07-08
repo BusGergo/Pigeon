ee448184ce2d9490b68f4d038e47486f1c78176e
Gergő-Vince Bús <bus.gergo@gmail.com>
2025-07-09 01:42:16 +0300

    Fixing parameter async issue

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,5 +1,3 @@
-function page({ params }: { params: { id: string } }) {
+export default async function page({ params }: { params: { id: string } }) {
 	return <div>{params.id}</div>;
 }
-
-export default page;
--- components/chat/ChatListItem.tsx
+++ components/chat/ChatListItem.tsx
@@ -1,12 +1,10 @@
-import type { Chat } from '@/types/chat';
 import Link from 'next/link';
+import type { Chat } from '@/types/chat';
 
-function ChatListItem({ chat }: { chat: Chat }) {
+export default function ChatListItem({ chat }: { chat: Chat }) {
 	return (
 		<Link href={`/chat/${chat.id}`}>
 			<h2>{chat.name}</h2>
 		</Link>
 	);
 }
-
-export default ChatListItem;
