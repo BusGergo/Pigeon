419b7fa69f6a0a60a393d02c3492492c297717e9
Gergő-Vince Bús <bus.gergo@gmail.com>
2025-08-20 01:53:27 +0300

    Progress with Auth UI

--- client/src/pages/auth/AuthView.tsx
+++ client/src/pages/auth/AuthView.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function AuthView() {
   return (
-    <div>
-      <Button>Click here</Button>
+    <div className="h-screen flex">
+      <div className="flex-1 bg-[url('auth-bg.jpg')] bg-cover bg-center rounded-3xl m-2 hidden md:flex"></div>
+      <div className="flex-1 bg-white flex flex-col justify-center items-center">
+        <Label>Email</Label>
+        <Input placeholder="Email" type="email" />
+
+        <Label>Password</Label>
+        <Input placeholder="Password" type="password" />
+      </div>
     </div>
   );
 }
